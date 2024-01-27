fe061607e6f5336938e0fe53ac9436d0d0db7ff5
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-01-27 19:44:53 +0200

    Add demo of useful library BenchamrkDotNet.

---
+++ BenchmarkDotNet/BenchmarkDotNet-Demo/BenchmarkDotNet-Demo/TestMethods.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet_Demo;
+
+[MemoryDiagnoser(false)]
+public class TestMethods
+{
+
+    [Benchmark]
+    [Arguments("IngreDientS", "inGreDientS")]
+    public bool Test2(string str1, string str2)
+       => str1.ToUpper() == str2.ToUpper();
+    [Benchmark]
+    [Arguments("IngreDientS", "inGreDientS")]
+    public bool Test1(string str1, string str2)
+        => string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase);
+
+}

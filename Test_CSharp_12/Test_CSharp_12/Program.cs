a2a46cb8984098e7c92ebd3882da3a7502c46289
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-03-21 22:37:08 +0200

    Add primary constructor demo.

--- Test_CSharp_12/Test_CSharp_12/Program.cs
+++ Test_CSharp_12/Test_CSharp_12/Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Buffers;
-
+﻿//Collection expressions
 List<int> arr1 = [1, 2, 3, 4, 5, 6];
 List<int> arr2 = [7, 8, 9, 10];
 int[] arr3 = [.. arr1, .. arr2];
 Console.WriteLine(string.Join(", ", arr3));
 
-//SearchValues<int> searchValues = SearchValues.Create();
 
+class Person(string name, int age)
+{
+    public string Name => name;
+    public int Age => age;
+}
 

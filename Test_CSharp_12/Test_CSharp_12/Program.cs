6a7dc0f988882a016ab0ce6839252a3c9e585235
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-03-21 22:26:12 +0200

    Added demo project C#12 new features.

---
+++ Test_CSharp_12/Test_CSharp_12/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Buffers;
+
+List<int> arr1 = [1, 2, 3, 4, 5, 6];
+List<int> arr2 = [7, 8, 9, 10];
+int[] arr3 = [.. arr1, .. arr2];
+Console.WriteLine(string.Join(", ", arr3));
+
+//SearchValues<int> searchValues = SearchValues.Create();
+
+

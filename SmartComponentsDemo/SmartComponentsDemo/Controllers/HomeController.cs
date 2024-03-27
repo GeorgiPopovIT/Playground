e4adc407c8c0ddea8208a9fd90494fe79947e3ff
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-03-27 22:10:23 +0200

    Add HangireDemo

---
+++ SmartComponentsDemo/SmartComponentsDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using SmartComponentsDemo.Models;
+using System.Diagnostics;
+
+namespace SmartComponentsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            BackgroundJob.Schedule(() => Console.WriteLine("Hangire"),TimeSpan.FromSeconds(2));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SmartComponentsDemo/SmartComponentsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartComponentsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

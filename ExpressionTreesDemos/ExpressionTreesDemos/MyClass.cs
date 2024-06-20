41267c29a596d98e883d409e69762e317cba3ac0
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-06-20 20:49:05 +0300

    Add first view over expression trees in C#.

---
+++ ExpressionTreesDemos/ExpressionTreesDemos/MyClass.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpressionTreesDemos;
+
+internal class MyClass
+{
+    public bool MyProperty { get; set; }
+
+    public string MyMethod(int number, string text) => number + text;
+}
---
+++ ExpressionTreesDemos/ExpressionTreesDemos/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExpressionTreesDemos;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+var myClass = new MyClass();
+
+Func<MyClass, string> func = c => c.MyMethod(42, "Test Expression Trees");
+
+Console.WriteLine(func(myClass));
+
+Func<int, int, int> sum = (x, y) => x + y;
+Console.WriteLine(sum(2, 4));
+
+Expression<Func<int>> add = () => 1 + 2;
+var func2 = add.Compile();
+Console.WriteLine(func2());
+
+Expression<Func<int, int>> addFive = (num) => num + 5;
+
+if (addFive is LambdaExpression lambdaExp)
+{
+    var parameter = lambdaExp.Parameters[1];
+
+    Console.WriteLine(parameter.Name);
+    Console.WriteLine(parameter.Type);
+}
+
+Expression<Func<MyClass, string>> expr = c => c.MyMethod(42, "Test Expression Trees");
+
+static void AnotherMethod(Func<MyClass,string> someFunc)
+{
+
+}

0c117f2ea562fd225f98dcf6fefc8db99fece19e
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-07-13 21:33:58 +0300

    Initialize broker connection in the constructor.

--- RabbitMQ_Demo/RabbitMQ_Demo/RabbitMQ/RabbitMQProducer.cs
+++ RabbitMQ_Demo/RabbitMQ_Demo/RabbitMQ/RabbitMQProducer.cs
@@ -6,16 +6,33 @@ namespace RabbitMQ_Demo.RabbitMQ;
 
 public class RabbitMQProducer : IMessageProducer
 {
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    public RabbitMQProducer()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost", // RabbitMQ server address
+            Port = 5672,            // Default port
+            UserName = "guest",     // Default username
+            Password = "guest"      // Default password
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
+
+
     public void SendMessage<T>(T message)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+      
+        //using var connection = factory.CreateConnection();
+        //using var channel = connection.CreateModel();
 
-        channel.QueueDeclare("orders");
+        _channel.ExchangeDeclare("orders",ExchangeType.Fanout);
 
         var json = JsonSerializer.Serialize(message);
         var body = Encoding.UTF8.GetBytes(json);
-        channel.BasicPublish(exchange: "", routingKey: "orders", body: body);
+        _channel.BasicPublish(exchange: "", routingKey: "orders", body: body);
     }
 }

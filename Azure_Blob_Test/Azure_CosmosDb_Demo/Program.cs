afbe241b4056f24c97f4f9f93709fb053efa6ea4
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-10-19 14:11:08 +0300

    Add test of cosmos db.

---
+++ Azure_Blob_Test/Azure_CosmosDb_Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure_CosmosDb_Demo;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+// Get variables, keys...
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+//var account = config["Account"];
+//var key = config["Key"];
+var databaseName = config["Database"];
+var containerName = config["Container"];
+var connectionString = config["ConnectionString"];
+
+//Initialize cosmos client, database, container
+var cosmosClient = new CosmosClient(connectionString);
+
+var database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+
+Container container = cosmosClient.GetContainer(databaseName, containerName);
+
+//create object
+
+//Team team = new(Guid.NewGuid().ToString(), "Slavia stadium", "Slavia stadium", DateTimeOffset.UtcNow, 21);
+
+Team team = new()
+{
+    Id = Guid.NewGuid().ToString(),
+    Name = "Slavia",
+    Stadium = "Slavia stadium",
+    CreatedDate = DateTimeOffset.UtcNow,
+    PlayersCount = 21
+};
+//add object to container in the db
+
+await container.CreateItemAsync<Team>(team, new PartitionKey(team.Id));
+
+//Read the object
+var result = await container.ReadItemAsync<Team>(team.Id, new PartitionKey(team.Id));
+
+Console.WriteLine(result.Resource.ToString());
---
+++ Azure_Blob_Test/Azure_CosmosDb_Demo/Team.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+//For some reason not work with Sytem.Text.Json yet.
+using System.Text.Json.Serialization;
+
+namespace Azure_CosmosDb_Demo;
+//case-sensitive
+//internal record Team(string id, string name, string stadium, DateTimeOffset createdDate, int playersCount);
+
+internal class Team
+{
+    [JsonProperty("id")]
+    public string? Id { get; init; }
+
+    [JsonProperty("name")]
+    public string? Name { get; init; }
+
+    [JsonProperty("stadium")]
+    public string? Stadium { get; init; }
+
+    [JsonProperty("createdDate")]
+    public DateTimeOffset CreatedDate { get; set; }
+
+    [JsonProperty("playersCount")]
+    public int PlayersCount { get; init; }
+}

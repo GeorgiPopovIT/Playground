54c74201c1db0d801c78665041f40c6f9230fa2a
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-12-22 19:22:33 +0200

    Started implementing domain layer from clean architecture. From course.

---
+++ CleanArchitecture/Bookify/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+    internal static readonly Currency None = new("");
+    private Currency(string currencyCode) => Code = currencyCode;
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code)
+            ?? throw new ApplicationException("The currency is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All = [Usd, Eur];
+}

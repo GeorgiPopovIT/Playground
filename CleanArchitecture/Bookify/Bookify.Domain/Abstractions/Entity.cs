0b598ba2dfdfd51ca795f875dd7c2a4cf2c83526
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-12-30 19:00:35 +0200

    Added application layer.

--- CleanArchitecture/Bookify/Bookify.Domain/Abstractions/Entity.cs
+++ CleanArchitecture/Bookify/Bookify.Domain/Abstractions/Entity.cs
@@ -2,5 +2,16 @@
 
 public abstract class Entity(Guid id)
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     public Guid Id { get; init; } = id;
+
+    public IReadOnlyCollection<IDomainEvent> GetDomainEvents()
+        => _domainEvents.ToList();
+
+    public void ClearDomainEvents()
+        => _domainEvents.Clear();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        => _domainEvents.Add(domainEvent);
 }
--- CleanArchitecture/Bookify/Bookify.Domain/Apartments/Apartment.cs
+++ CleanArchitecture/Bookify/Bookify.Domain/Apartments/Apartment.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Shared;
 
 namespace Bookify.Domain.Apartments;
 
@@ -31,7 +32,7 @@ public sealed class Apartment : Entity
 
     public Money CleaningFee { get; private set; }
 
-    public DateTimeOffset? LasBookedOnUtc { get; private set; }
+    public DateTimeOffset? LastBookedOnUtc { get; internal set; }
 
     public List<Amenity> Amenities { get; private set; } = [];
 }

04be41952c5afb75178f7ca8d10c7955331d89ef
GeorgiPopovIT <gogopopo26062003@gmail.com>
2025-05-14 22:51:21 +0300

    Implemeted basic exchange fanout in rabbit mq with one producer and two consumers,

---
+++ RabbitMQ_Demo/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "messages",
+    durable: true,
+    autoDelete: false,
+    type: ExchangeType.Fanout);
+
+await Task.Delay(10000);
+
+for (int i = 0; i < 10; i++)
+{
+    string message = $"{DateTime.UtcNow} - {Guid.CreateVersion7()}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    //await channel.BasicPublishAsync(
+    //    exchange: string.Empty,
+    //    routingKey: "message",
+    //    body: body,
+    //    basicProperties: new BasicProperties { Persistent = true},
+    //    mandatory: true);
+
+    await channel.BasicPublishAsync(
+        exchange: "messages",
+        routingKey: string.Empty,
+        body: body,
+        basicProperties: new BasicProperties { Persistent = true },
+        mandatory: true);
+
+    Console.WriteLine($"Sent {message}");
+
+    await Task.Delay(2000);
+}
---
+++ RabbitMQ_Demo/RabbitMq.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Consumer One");
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "messages",
+    durable: true,
+    autoDelete: false,
+    type: ExchangeType.Fanout);
+
+await channel.QueueDeclareAsync(
+    queue: "message-1",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+
+await channel.QueueBindAsync(queue: "message-1", exchange: "messages", routingKey: string.Empty);
+
+Console.WriteLine("Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {message}");
+    
+    await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+};
+
+await channel.BasicConsumeAsync("message-1", autoAck: false, consumer: consumer);
+
+Console.ReadLine();
